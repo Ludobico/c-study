b5060ec6109532dde59e4972b5b41ac7e4c93810
Ludobico <aqs450@gmail.com>
2023-02-27 15:56:57 +0900

    c#

---
+++ C_structure/04.Var.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Var
+{
+    static void Main()
+    {
+        var name = "c#"; // string 형식으로 변환
+        Console.WriteLine(name);
+
+        var version = 8.0; // double 형식으로 변환
+        Console.WriteLine("{0:0.0}", version);
+    }
+}
