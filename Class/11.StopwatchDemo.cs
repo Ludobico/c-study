541d9e29797fa4628e06ff10f8833e1bcc8547a1
김찬진 <aqs450@gmail.com>
2023-03-07 06:00:14 +0000

    c# class

---
+++ Class/11.StopwatchDemo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+class StopwatchDemo{
+	static void Main(){
+		Stopwatch timer = new Stopwatch();
+		timer.Start();
+		LongTimeProcess();
+		timer.Stop();
+		
+		//밀리초 단위로 표시
+		Console.WriteLine("경과 시간 : {0}밀리초", timer.Elapsed.TotalMilliseconds);
+		
+		// 초 단위로 표시
+		Console.WriteLine("경과 시간 : {0}초", timer.Elapsed.Seconds);
+	}
+	
+	static void LongTimeProcess(){
+		//3초간 대기
+		Thread.Sleep(3000);
+	}
+}
